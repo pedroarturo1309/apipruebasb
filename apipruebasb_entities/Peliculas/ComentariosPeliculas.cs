8fc7e1921eb0b3da22373b2ee655b7d0c542ca40
Pedro Arturo de Leon Parra <deleonarturo09@gmail.com>
2023-11-14 14:51:58 -0400

    crear clase comentarios peliculas

---
+++ apipruebasb_entities/Peliculas/ComentariosPeliculas.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apipruebasb_entities.Peliculas
+{
+    public class ComentariosPeliculas
+    {
+        
+    }
+}
